de32f4916437cef8774bb50eef96ba36161afcbc
NikhilShriwas77 <cpaknikhil@gmail.com>
2024-09-12 11:37:57 +0530

    Update Music_Store_Query.sql
    question practices

--- Music Playlist- SQL/Music_Store_Query.sql
+++ Music Playlist- SQL/Music_Store_Query.sql
@@ -1,5 +1,3 @@
-/*	Question Set 1 - Easy */
-
 /* Q1: Who is the senior most employee based on job title? */
 
 SELECT title, last_name, first_name 
@@ -225,6 +223,4 @@ WHERE cc.total_spending = ms.max_spending
 ORDER BY 1;
 
 
-/* source: www.youtube.com/@RishabhMishraOfficial */
 
-/* Thank You :) */
